86840c901de0cb58fde524b3c2e47e47fa9b684a
devendra <devekr12@gmail.com>
2021-12-31 00:36:16 +0530

    ToDo list done without UI

--- src/Components/ListItems.jsx
+++ src/Components/ListItems.jsx
@@ -1,11 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import './listItem.css'
 
 function ListItems(props) {
 
+    const [toEdit, setEdit] = useState(false)
+    const [newItem, setNewItem] = useState("")
+
+    const clickHandler = () => {
+        setEdit(true)
+    }
+
+
+    const onChangeHandler = (event) => {
+        setNewItem(event.target.value)
+    }
+
+    const saveHandler = () => {
+        props.edit(newItem, props.idx)
+    }
+
     return (
             <>
-                <button onClick={() => {props.edit("edited" ,props.idx)}}>edit</button>
-                <button onClick={() => {props.remove(props.idx)}}>Remove</button>
+                {toEdit 
+                    ?   <span>
+                            <input 
+                                onChange={(event) => onChangeHandler(event)}
+                                placeholder='new item'
+                            />
+                            <button onClick={saveHandler} id='save--btn'>
+                                📝
+                            </button>
+                        </span>
+                    : <button id='edit--btn' onClick={clickHandler}>✏️</button>
+                }            
+                
+                <button id='remove--btn' onClick={() => {props.remove(props.idx)}}>❎</button>
             </>
     )
 }
